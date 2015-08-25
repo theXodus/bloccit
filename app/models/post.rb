5aa82ce3fc79aff65c83fc9095a465cd1229896e
jprice1990 <jprice1990@gmail.com>
2015-08-25 21:52:39 +0000

    Checkpoint 32 completed, CRUD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,3 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  
-  def self.all_with_censored
-    all.each_with_index do |post, i|
-      if i % 5 == 0
-        post.title = 'CENSORED'
-      end
-    end
-  end
 end
