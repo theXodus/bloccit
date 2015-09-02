553cca4428fb02b13e3644829a92ced58c4e660b
jprice1990 <jprice1990@gmail.com>
2015-09-02 02:28:50 +0000

    saving progress, controllers and model created, routes compeleted

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,14 @@
+class AdvertisementsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @advertisement = Advertisement.new
+  end
+
+  def create
+  end
+end
