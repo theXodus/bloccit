bc8075977597e3704545f54e7b361f5e0a14fd75
jprice1990 <jprice1990@gmail.com>
2015-09-02 20:07:40 +0000

    Assignment 33 completed, questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
   
+  get 'questions/index'
+
+  get 'questions/new'
+
+  get 'questions/create'
+
+  get 'questions/show'
+
+  get 'questions/edit'
+
+  get 'questions/update'
+
+  get 'questions/destroy'
+
   resources :posts
-  
+  resources :questions
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+   let(:question) { Question.create!(title: "New Post Title", body: "New Post Body") }
+  
+  context "attributes" do
+     
+     it "should respond to title" do
+        expect(question).to respond_to(:title) 
+     end
+     
+     it "should respond to body" do
+        expect(question).to respond_to(:body) 
+     end
+     
+  end
+end
