b5f6bf96cbe5c75fc6961ca735795ad2a3051c7b
jprice1990 <jprice1990@gmail.com>
2015-09-08 22:22:23 +0000

    Checkpoint 35 completed, validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,6 +8,11 @@ let(:topic) { Topic.create!(name: RandomData.random_sentence, description: Rando
 let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
 
 it { should belong_to(:topic) }
+it { should validate_presence_of(:title) }
+it { should validate_presence_of(:body) }
+it { should validate_presence_of(:topic) }
+it { should validate_length_of(:title).is_at_least(5) }
+it { should validate_length_of(:body).is_at_least(20) }
 
   context "attributes" do
      
