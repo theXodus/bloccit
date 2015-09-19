71d5551a85e64d4310464387aec83392be38ea58
jprice1990 <jprice1990@gmail.com>
2015-09-19 02:30:02 +0000

    Add yes/no buttons to confirm view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def confirm
+  def create
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash[:error] = "There was an error creating your account. Please try again."
+      render :new
+    end
   end
   
   def create
