e09de20fa06f65e888077d6eafca411ac6e8026e
jprice1990 <jprice1990@gmail.com>
2015-09-28 16:43:34 +0000

    Checkpoint 45 completed, comments

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,7 @@ let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: Random
 
 it { should belong_to(:topic) }
 it { should belong_to(:user) }
+it { should have_many(:comments) }
 it { should validate_presence_of(:title) }
 it { should validate_presence_of(:body) }
 it { should validate_presence_of(:topic) }
