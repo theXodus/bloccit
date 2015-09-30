ffb1258509b86d0b7de4be7393c847c828a16c74
jprice1990 <jprice1990@gmail.com>
2015-09-30 21:26:59 +0000

    Checkpoint 46 complete, labels

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :labels, only: [:show]
+
   resources :topics do
     resources :posts, expect: [:index]
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,6 +7,9 @@ let(:topic) { Topic.create!(name: RandomData.random_sentence, description: Rando
 let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
 let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
 
+it { should have_many(:labelings) }
+it { should have_many(:labels).through(:labelings) }
+
 it { should belong_to(:topic) }
 it { should belong_to(:user) }
 it { should have_many(:comments) }
